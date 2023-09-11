43be9accd82a822be2e34383b772935c5d23ba0a
AmrBarakat8694 <amrbarakat27@github.com>
2023-09-11 15:32:29 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#imclude < stdio.h >
+/**
+ * main - A program that print a line using the printf fimction
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
+
